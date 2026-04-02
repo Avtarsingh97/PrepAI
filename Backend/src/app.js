93dac12bfa6ebc888a903b081864727a48e51411
Avtar Singh <officialavtar13@gmail.com>
2026-04-02 19:31:03 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: [process.env.FRONTEND_URL],
     credentials: true
 }));
 
